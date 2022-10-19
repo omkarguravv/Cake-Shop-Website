df2ab9d0055e0f97279fbd4f6569ee046c07a07c
OmkarGurav <omkargurav1920@gmail.com>
2022-10-19 16:50:24 +0530

    11- redux-Store for client created❌

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,21 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './admin/store/AdminIndex';
+import store2 from './client/store';
 
 window.store = store
+window.store = store2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store={store }>
+  <Provider store={store2 }>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
+  </Provider>
   </Provider>
 );
 
